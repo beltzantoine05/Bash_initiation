a3d5986d3d3f7f9d78d68fe8d7abe05899e50b19
beltzantoine05 <beltzantoine05@gmail.com>
2025-09-20 19:04:53 +0200

    Correction de decimal_to_binary.sh

--- decimal_to_binary.sh
+++ decimal_to_binary.sh
@@ -6,7 +6,7 @@ for nombre in $@; do
 	binaire="$reste$binaire"  # Ajouter le reste au début de la chaîne binaire
 	nombre=$((nombre / 2))  # Diviser le nombre par 2
     done
-    echo "Le nombre binaire de $decimal est : $binaire"
+    echo Le nombre binaire de $nombre est : $binaire
 
 done
 
